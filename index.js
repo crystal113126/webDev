3a5d3610eb321c6921045efcb05cd3922bdcee30
Jingia <jingx113126@gmail.com>
2023-02-15 22:25:11 -0500

    add js function for game logical

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6 )+ 1; // 1-6
+var randomNumber1 = Math.floor(Math.random() * 6)+ 1; // 1-6
 let randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 let randomImageSource = "images/" + randomDiceImage; // get the dir images/dice1.png-images/dice6.png
 
 let image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6)+ 1; // 1-6
+let randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+let randomImageSource2 = "images/" + randomDiceImage2; // get the dir images/dice1.png-images/dice6.png
+
+
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "#Play 1 wins!";
+} else {
+    document.querySelector("h1").innerHTML = "#Play 2 wins";
+}
